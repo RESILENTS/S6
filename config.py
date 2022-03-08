3b80b576909884f13534c2738af2e5b9aee3a791
RESILENTS <60385275+RESILENTS@users.noreply.github.com>
2022-03-08 21:58:12 +0100

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,22 @@ curdir = os.curdir
 bot = telebot.TeleBot('5298338850:AAHKVHFUJrFCTJ3qmNrWDK31KgFiYU91YIg') #токен бота
 
 admins = [641892529, 1938749145] #айди админов
+
 channel_id = -1001450252060
+
+phone = '' #номер киви
+
+token = '' #токен киви с первыми 3 галочками
+
+price24 = 2990 #цена 2.4+ версии
+
+price3duo = 4990 #цена 3дуо версии
+
+pricemulti = 2490 #цена 3мульти версии
+
+pricestick = 150 #цена за 1 пачку стиков
+
+telegram = '@faulmit' #никнейм телеграмм
 
 id1 = []
 
@@ -44,3 +59,39 @@ def get_usersId_banker():
 def bill_create(length):
     letters = string.ascii_lowercase
     return ''.join(random.choice(letters) for i in range(length))
+
+
+def deposit(chat_id, price):
+    try:
+        chat_id = chat_id
+        global amount
+        amount = price
+        billId = str(f'{bill_create(6)}_{random.randint(10000, 999999)}')
+
+        inline_keyboard = types.InlineKeyboardMarkup(row_width=1)
+        inline_1 = types.InlineKeyboardButton(text="Проверить оплату",
+                                                  callback_data=f'STATUS-{billId}-{amount}')
+        inline_keyboard.add(inline_1)
+        message = bot.send_message(chat_id,
+                                       f'💁🏻‍♀️ *Переведите* {str(amount)} ₽ на QIWI\nСчет действителен *10* минут\n\nНомер: `+{phone}`\nКомментарий: `{billId}`\n\n_Нажмите на реквизиты и комментарий чтобы их скопировать_',
+                                       parse_mode="Markdown",
+                                       reply_markup=inline_keyboard)
+
+
+    except Exception as e:
+        print(e)
+
+
+def user_status_pay(call, billId, amount):
+    chat_id = call.message.chat.id
+    api = QApi(phone=phone, token=token)
+    payments = api.payments['data']
+    for info_payment in payments:
+        if info_payment['comment'] == billId:
+            if str(amount) == str(info_payment['sum']['amount']):
+                bot.send_message(chat_id, '✅Оплата прошла успешно!✅')
+                bot.delete_message(chat_id=call.message.chat.id, message_id=call.message.message_id)
+
+            else:
+                bot.answer_callback_query(callback_query_id=call.message.id, show_alert=False,
+                                          text="💁🏻‍♀️ Платеж не найден")
